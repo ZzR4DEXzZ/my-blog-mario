e144d6cfdadde266caf838d9ad255af490608f5b
Mario <marioslavkovski@gmail.com>
2023-05-10 12:22:59 +0200

    vite.config config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/pinia-json-dynamic-blog/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/my-blog-mario/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
